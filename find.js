9498004afce5f91376998d676e211b191fe30c42
Bayan Berri <bberri@stuy.edu>
2017-12-13 12:46:24 -0500

    exponents

--- find.js
+++ find.js
@@ -13,13 +13,19 @@ console.log( "box width: " + boxWidth );
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
-    return Math.sqrt((x1-x0)*(x1-x0) + (y1-y0)*(y1-y0));
+  return ((((x1-x0)**2) + ((y1-y0)**2))**.5);
 };
+
 
 
 var findIt = function(e) {
-    console.log(e.X,e.Y);
+    console.log(e.clientX,e.clientY);
 };
+
+console.log("mousex: ");
+findIt();
+console.log("distance:"+ distance(e.X,e.Y,targetX,targetY));
+
 
 /*
 your OTHER FXNS
@@ -31,3 +37,4 @@ box.addEventListener("mousemove", findIt);
 event : 'mousemove'
 e.X //xcor of mouse
 e.y //ycor of mouse
+*/
