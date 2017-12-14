bbe509a5cd250f508f7e6b44b119b8389cb3a08d
Ibnul Jahan <ijahan1@stuy.edu>
2017-12-14 07:30:36 -0500

    tweaks

--- find.js
+++ find.js
@@ -11,10 +11,17 @@ var arr = [0,0];
 var dist=0;
 var maxDistance=0;
 
+var points = 0;
+
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
 
 //console.log(e.clientX,e.clientY);
+
+var randomizeTarget = function(e) {
+    targetX = Math.random()*boxWidth;
+    targetY = Math.random()*boxHeight;
+}
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
@@ -22,7 +29,7 @@ var distance = function (x0, y0, x1, y1) {
 };
 
 
-
+//logs the current position of the mouse, and also returns it
 var findIt = function(e) {
   arr=[e.x,e.y];
   console.log(arr[0],arr[1]);
@@ -30,35 +37,54 @@ var findIt = function(e) {
   return arr;
 };
 
+//logs the current distance of the mouse from the target, and also returns it
 var findDistance = function(e){
   dist = distance(arr[0],arr[1],targetX,targetY);
 	console.log("distance: " + distance(arr[0],arr[1],targetX,targetY));
   return dist;
 }
 
+//Event Listeners to constantly track the position of the mouse and it's distance from the target
 box.addEventListener("mousemove", findIt);
 box.addEventListener("mousemove", findDistance);
 
+//The lightness of the color is based on the ratio of the current distance to the maximum distance
 var addColor = function(e){
   dist= distance(arr[0],arr[1],targetX,targetY);
   farthest=distance(0,0,boxWidth,boxHeight)/2;
   lightness=(dist/farthest) *100;
-  console.log("lightness: "+ lightness);
+    console.log("lightness: "+ lightness);
+    if (lightness < 3) {
+	console.log("You should click now!");
+    }
   box.setAttribute("style", "background-color: hsl(175,100%,"+(100-lightness)+"%)");
   
 };
+
+//Sends an alert if you click while close enough to the target
+//Automatically reloads after the user clicks okay
 var found= function(e){
-  if(lightness<3){
-    alert("Good work!!")//js pop up
-  }
+    if(lightness<3){
+	points += 1;
+	console.log("Good work! Score: " + points);
+	//alert spawns an alert box
+	if(alert("Good work! Score: " + points)){}
+	//when the alert is closed, the target is changed
+	else randomizeTarget; 
+    }
 };
+
+//Event Listeners to change the color when the mouse moves, and to display the alert when the target is found
 box.addEventListener("mousemove", addColor);
 box.addEventListener("click", found);
-                     
+
+
+
+
 /*
 event : 'mousemove'
-e.X //xcor of mouse
-e.Y //ycor of mouse
+e.x //xcor of mouse
+e. y//ycor of mouse
 */
 /*
   NOTES
