a43c7ba8af2fe04c1d94a9a6318eb5b9b075d81b
Bayan Berri <bberri@stuy.edu>
2017-12-14 07:37:19 -0500

    comments and console statements

--- find.js
+++ find.js
@@ -2,19 +2,17 @@ var box = document.getElementById("box");
 var boxHeight = box.offsetHeight;
 var boxWidth = box.offsetWidth;
 
-//hardcode target as center
-//randomize later
+//Randomly sets the target upon each page refresh.
 var targetX = Math.random()*boxWidth;
 var targetY = Math.random()*boxHeight;
-
-var arr = [0,0];
-var dist=0;
-var maxDistance=0;
+//initializing variables 
+var arr = [0,0];//array of coordinates
+var dist=0;//distance from the target 
+var maxDistance=0;//used with dist to create a percentage
 
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
 
-//console.log(e.clientX,e.clientY);
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
@@ -22,14 +20,14 @@ var distance = function (x0, y0, x1, y1) {
 };
 
 
-
+//returns array of the coordinates [x,y] of the mouse
 var findIt = function(e) {
   arr=[e.x,e.y];
-  console.log(arr[0],arr[1]);
-  //console.log(arr);
+  console.log("Mouse coordinates:");
+  console.log("("+ arr[0]+","+arr[1]+")");
   return arr;
 };
-
+//returns the distance from the mouse to the target
 var findDistance = function(e){
   dist = distance(arr[0],arr[1],targetX,targetY);
 	console.log("distance: " + distance(arr[0],arr[1],targetX,targetY));
@@ -38,17 +36,18 @@ var findDistance = function(e){
 
 box.addEventListener("mousemove", findIt);
 box.addEventListener("mousemove", findDistance);
-
+//creates a ratio of distance to farthese distance 
 var addColor = function(e){
   dist= distance(arr[0],arr[1],targetX,targetY);
-  farthest=distance(0,0,boxWidth,boxHeight)/2;
-  lightness=(dist/farthest) *100;
+  maxDistance=distance(0,0,boxWidth,boxHeight)/2;
+  lightness=(dist/maxDistance) *100;
   console.log("lightness: "+ lightness);
-  box.setAttribute("style", "background-color: hsl(175,100%,"+(100-lightness)+"%)");
+  box.setAttribute("style", "background-color: hsl(175,100%,"+(100-lightness)+"%)")//uses ratio to set Attribute
   
 };
+//hooray!
 var found= function(e){
-  if(lightness<3){
+  if(lightness<5){//if the percent is <5 then it's at the right place and you win!!
     alert("Good work!!")//js pop up
   }
 };
